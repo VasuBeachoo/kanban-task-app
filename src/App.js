5b1d45132ad359d8ec86b69cbc814fe1ee8865a5
VasuBeachoo <vasubeachoo@gmail.com>
2023-01-08 10:43:15 -0500

    Create Kanban task app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+
+const AppBox = styled.div``;
+
+const App = () => {
+  return (
+    <AppBox>
+      <GlobalStyle />
+    </AppBox>
+  );
+};
+
+export default App;
